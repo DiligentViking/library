2d9a624087d6a2d7f466341e0fbe3979bb85b8fe
DiligentViking <acore-n@proton.me>
2025-04-29 23:06:05 -0500

    Add main library logic

--- script.js
+++ script.js
@@ -1,18 +1,38 @@
 const myLibrary = [];
 
-function Book(id, title, author, genre, numPages) {  // I am kind of unsure why TOP wants the params in the second func
-  this.id = id;
+function Book(title, author, genre, numPages, id) {  // I am kind of unsure why TOP wants the params in the second func
   this.title = title;
   this.author = author;
   this.genre = genre;
   this.numPages = numPages;
+  this.id = id;
 }
 
 function addBookToLibrary(title, author, genre, numPages) {
   const uuid = self.crypto.randomUUID();
-  const book = new Book(uuid, title, author, genre, numPages);
+  const book = new Book(title, author, genre, numPages, uuid);
   myLibrary.push(book);
 }
 
-addBookToLibrary('The Guide', 'Brian Brendon', 'self-help', 100);
+addBookToLibrary('The Guide', 'Brian Brendon', 'Self-Help', 100);
+addBookToLibrary('Macbeth', 'William Shakespeare', 'Tragedy', 80);
 console.log(myLibrary);
+
+
+const tableData = document.querySelector('#table-data');
+
+for (const book of myLibrary) {
+  const newRow = document.createElement('tr');
+
+  const dataTitle = document.createElement('th');
+  dataTitle.textContent = book.title;
+  for (const prop in book) {
+    if (prop == 'title') continue;
+    const dataOther = document.createElement('td');
+    dataOther.textContent = book[prop];
+    newRow.append(dataOther);
+  }
+  newRow.prepend(dataTitle);
+  
+  tableData.appendChild(newRow);
+}
